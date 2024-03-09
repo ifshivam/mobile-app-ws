118f37624436a32b335839eb400b25145c9b105c
ifshivam <shivamrai0307@gmail.com>
2024-03-09 22:34:07 +0530

    added userRest model

---
+++ src/main/java/com/learning/mobileappws/ui/model/response/UserRest.java
@@ -0,0 +1,15 @@
+package com.learning.mobileappws.ui.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRest {
+
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String mail;
+
+}
