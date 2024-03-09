5f80d9f29abb516884b3c13945034551f3d6594e
ifshivam <shivamrai0307@gmail.com>
2024-03-09 23:38:15 +0530

    added getter and setter

--- src/main/java/com/learning/mobileappws/ui/model/request/UserRequestModel.java
+++ src/main/java/com/learning/mobileappws/ui/model/request/UserRequestModel.java
@@ -1,5 +1,10 @@
 package com.learning.mobileappws.ui.model.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserRequestModel {
     private String userId;
     private String firstName;
