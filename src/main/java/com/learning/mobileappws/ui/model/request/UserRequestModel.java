3ef4ab53bcfc08a8dc6119053aebb729ee102349
ifshivam <shivamrai0307@gmail.com>
2024-03-09 23:36:12 +0530

    created UserRequestModel class

---
+++ src/main/java/com/learning/mobileappws/ui/model/request/UserRequestModel.java
@@ -0,0 +1,8 @@
+package com.learning.mobileappws.ui.model.request;
+
+public class UserRequestModel {
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String mail;
+}
