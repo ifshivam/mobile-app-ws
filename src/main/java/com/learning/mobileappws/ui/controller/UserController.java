96b21f6e98fb3546dfcb96e90e054f55694bcfb8
ifshivam <shivamrai0307@gmail.com>
2024-03-10 00:32:22 +0530

    added validation in project

--- src/main/java/com/learning/mobileappws/ui/controller/UserController.java
+++ src/main/java/com/learning/mobileappws/ui/controller/UserController.java
@@ -1,24 +1,55 @@
 package com.learning.mobileappws.ui.controller;
 
+import com.learning.mobileappws.ui.model.request.UserRequestModel;
+import com.learning.mobileappws.ui.model.response.UserRest;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    @GetMapping("/{userId}")
-    public String getUser(@PathVariable String userId){
-        return "x";
+    @GetMapping(value = "/{userId}",produces = {
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_JSON_VALUE
+})
+    public ResponseEntity<UserRest> getUser(@PathVariable String userId){
+        UserRest userRest = new UserRest();
+        userRest.setUserId(userId);
+        userRest.setFirstName("shivam");
+        userRest.setLastName("Rai");
+        userRest.setMail("shivam@mail.com");
+        return new ResponseEntity<UserRest>(userRest,HttpStatus.OK);
     }
-    @PostMapping("/")
-    public String createUser(){
-        return "x";
+    @GetMapping
+    public String getUsers(@RequestParam(value = "page",defaultValue = "1")int page,
+                           @RequestParam(value = "limit",defaultValue = "50")int limit,
+                           @RequestParam(value = "sort",defaultValue = "desc",required = false)String sort){
+        return "getUsersCalled";
+
+    }
+    @PostMapping(consumes = {
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_JSON_VALUE
+    },produces = {
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_JSON_VALUE
+    })
+    public ResponseEntity<UserRest> createUser(@Valid @RequestBody UserRequestModel userRequestModel){
+        UserRest userRest = new UserRest();
+        userRest.setFirstName(userRequestModel.getFirstName());
+        userRest.setLastName(userRequestModel.getLastName());
+        userRest.setMail(userRequestModel.getMail());
+        return new ResponseEntity<>(userRest,HttpStatus.CREATED);
     }
     @PutMapping()
     public String updateUser(){
         return "x";
     }
-    @DeleteMapping
-    public String deleteUser(){
+    @DeleteMapping()
+    public String deleteUser(@RequestParam(value="userId") Long userId){
         return "x";
     }
 }
--- src/main/java/com/learning/mobileappws/ui/model/request/UserRequestModel.java
+++ src/main/java/com/learning/mobileappws/ui/model/request/UserRequestModel.java
@@ -1,13 +1,23 @@
 package com.learning.mobileappws.ui.model.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserRequestModel {
-    private String userId;
+    @NotBlank(message = "first name can not be empty or null")
+    @Size(min=2,message = "provide valid first name")
     private String firstName;
+    @Size(min=2,message = "provide valid last name")
     private String lastName;
+    @Email
     private String mail;
+    @NotNull(message = "password can not be null")
+    @Size(min=7,max = 16,message = "password must be equal or greater than 7 and less than or equal to 16 character")
+    private String password;
 }
